b67977a02d9881a7f33e4d1fb766c35af2c8761e
Mrunal Deo <mrunaldeo16@gmail.com>
2024-08-16 20:20:19 +0530

    final

--- assignment/client/src/App.js
+++ assignment/client/src/App.js
@@ -1,1 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Project from './pages/Project';
+import Dashboard from './pages/Dashboard';
+import AllRoutes from './components/AllRoutes';
 
+function App() {
+  return (
+    <div className='container'>
+      
+      <AllRoutes/>
+    </div>
+  );
+}
+
+export default App;
